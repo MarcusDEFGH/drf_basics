82dbaa431534bc3db10dfd1c4d2628655573817b
Marcus <marcusvini211@gmail.com>
2020-04-19 16:57:29 -0300

    Adds pagination setting

--- drf_basics/settings.py
+++ drf_basics/settings.py
@@ -143,5 +143,8 @@ MEDIA_ROOT = 'images'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS':
-    ('django_filters.rest_framework.DjangoFilterBackend',)
+    ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
